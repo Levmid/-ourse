37295ccfa1c0bc8835a5832b02db639c7b5baeb4
Levmid <didi200345@gmail.com>
2025-02-01 01:19:28 +0300

    update 1

--- src/com/lesson2/CompueterRunner.java
+++ src/com/lesson2/CompueterRunner.java
@@ -5,10 +5,14 @@ public class CompueterRunner {
         Computer computer1 = new Computer();
         computer1.printLoad();
 
+        //ctr + p - можем увидить все перегрузки метода
+
         Computer computer2 = new Computer(1050);
         computer2.printLoad();
 
         Computer computer3 = new Computer(1003, 12);
-        computer3.printLoad();//
+        computer3.printLoad();
+        computer3.load(true
+        );
     }
 }
--- src/com/lesson2/Computer.java
+++ src/com/lesson2/Computer.java
@@ -2,30 +2,38 @@ package com.lesson2;
 
 public class Computer {
     //Поля класса
-    int ssd = 500;
-    int rem = 1024;
+    private int ssd = 500;
+    private int rem = 1024;
 
     Computer(){
         System.out.println("Я был создан");
     }
 
-    Computer(int newSsd){
-        ssd = newSsd;
+    Computer(int ssd){
+        this.ssd = ssd;
     }
 
-    Computer(int newSsd, int newRem){
-        ssd = newSsd;
-        rem = newRem;
+    Computer(int ssd, int rem){
+        this.ssd = ssd;
+        this.rem = rem;
     }
 
     void load() {
         System.out.println("Я загрузился! " + ssd + " - Моя память "+ rem + " - Мой жесткий диск");
     }
 
+    void load(boolean open){
+        System.out.println("Я загрузился");
+        if(open){
+            System.out.println("Я открыл крышку");
+        }
+    }
+
+
     void printLoad(){
         System.out.println(ssd + " - Моя память ");
         System.out.println(rem + " - Мой жесткий диск");
-        System.out.println( );
+        System.out.println();
     }
 
 }
